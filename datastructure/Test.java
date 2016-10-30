4b32fb0ff8b1e5930bb6cf73be34234ca4d3d71d
Rohit Negi <rohit25.negi@gmail.com>
2016-10-30 17:03:15 +0530

    this is first working commit

--- datastructure/Test.java
+++ datastructure/Test.java
@@ -1,8 +1,17 @@
-
-class Test
+import java.util.Scanner;
+import javax.swing.*;
+import java.awt.event.*;
+class Test extends JFrame
 {
 	public static void main(String arg[])
 	{
-		System.out.println(new Expression("(10 + 21 ) * 2",0).evaluate());
+		new Test("calculator");
+	}
+	Test(String str)
+	{
+		super(str);
+		setBounds(100,100,300,400);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
 	}
 }
